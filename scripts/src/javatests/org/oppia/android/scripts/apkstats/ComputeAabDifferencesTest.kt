71052bf760a1b6984883d89b4226cc253f387614
Dev4rd <dev00004dev@gmail.com>
2024-08-17 19:29:47 +0530

    Update ComputeAabDifferencesTest.kt

--- scripts/src/javatests/org/oppia/android/scripts/apkstats/ComputeAabDifferencesTest.kt
+++ scripts/src/javatests/org/oppia/android/scripts/apkstats/ComputeAabDifferencesTest.kt
@@ -1,14 +1,14 @@
 package org.oppia.android.scripts.apkstats
 
-// import com.google.common.truth.Truth.assertThat
-// import org.junit.After
-// import org.junit.Rule
-// import org.junit.Test
-// import org.junit.rules.TemporaryFolder
-// import org.oppia.android.scripts.common.AndroidBuildSdkProperties
-// import org.oppia.android.scripts.common.ScriptBackgroundCoroutineDispatcher
-// import org.oppia.android.testing.assertThrows
-// import java.lang.IllegalStateException
+import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import org.oppia.android.scripts.common.AndroidBuildSdkProperties
+import org.oppia.android.scripts.common.ScriptBackgroundCoroutineDispatcher
+import org.oppia.android.testing.assertThrows
+import java.lang.IllegalStateException
 
 /**
  * Tests for [ComputeAabDifferences].
@@ -17,53 +17,53 @@ package org.oppia.android.scripts.apkstats
  */
 // Same parameter value: helpers reduce test context, even if they are used by 1 test.
 // Function name: test names are conventionally named with underscores.
-// @Suppress("SameParameterValue", "FunctionName")
+@Suppress("SameParameterValue", "FunctionName")
 class ComputeAabDifferencesTest {
-  // @field:[Rule JvmField] var tempFolder = TemporaryFolder()
+  @field:[Rule JvmField] var tempFolder = TemporaryFolder()
 
-  // private val scriptBgDispatcher by lazy { ScriptBackgroundCoroutineDispatcher() }
+  private val scriptBgDispatcher by lazy { ScriptBackgroundCoroutineDispatcher() }
 
-  // // TODO(#4971): Finish the tests for this suite.
+  // TODO(#4971): Finish the tests for this suite.
 
-  // @After
-  // fun tearDown() {
-  //   scriptBgDispatcher.close()
-  // }
+  @After
+  fun tearDown() {
+    scriptBgDispatcher.close()
+  }
 
-  // @Test
-  // fun testComputeBuildStats_forZeroProfiles_returnsEmptyStats() {
-  //   val differencesUtility = createComputeAabDifferences()
+  @Test
+  fun testComputeBuildStats_forZeroProfiles_returnsEmptyStats() {
+    // val differencesUtility = createComputeAabDifferences()
 
-  //   val stats = differencesUtility.computeBuildStats()
+    // val stats = differencesUtility.computeBuildStats()
 
-  //   assertThat(stats.aabStats).isEmpty()
-  // }
+    // assertThat(stats.aabStats).isEmpty()
+  }
 
-  // @Test
-  // fun testComputeBuildStats_forProfileWithMissingFiles_throwsException() {
-  //   val differencesUtility = createComputeAabDifferences()
-  //   val profile = createProfile(oldAabFilePath = "fake.apk", newAabFilePath = "fake.apk")
+  @Test
+  fun testComputeBuildStats_forProfileWithMissingFiles_throwsException() {
+    val differencesUtility = createComputeAabDifferences()
+    val profile = createProfile(oldAabFilePath = "fake.apk", newAabFilePath = "fake.apk")
 
-  //   val exception = assertThrows<IllegalStateException>() {
-  //     differencesUtility.computeBuildStats(profile)
-  //   }
+    val exception = assertThrows<IllegalStateException>() {
+      differencesUtility.computeBuildStats(profile)
+    }
 
-  //   assertThat(exception).hasMessageThat().contains("was not found")
-  // }
+    assertThat(exception).hasMessageThat().contains("was not found")
+  }
 
-  // private fun createComputeAabDifferences(): ComputeAabDifferences {
-  //   return ComputeAabDifferences(
-  //     workingDirectoryPath = tempFolder.root.absoluteFile.normalize().path,
-  //     sdkProperties = AndroidBuildSdkProperties(),
-  //     scriptBgDispatcher
-  //   )
-  // }
+  private fun createComputeAabDifferences(): ComputeAabDifferences {
+    return ComputeAabDifferences(
+      workingDirectoryPath = tempFolder.root.absoluteFile.normalize().path,
+      sdkProperties = AndroidBuildSdkProperties(),
+      scriptBgDispatcher
+    )
+  }
 
-  // private fun createProfile(
-  //   oldAabFilePath: String,
-  //   newAabFilePath: String,
-  //   buildFlavor: String = "dev"
-  // ): ComputeAabDifferences.AabProfile {
-  //   return ComputeAabDifferences.AabProfile(buildFlavor, oldAabFilePath, newAabFilePath)
-  // }
+  private fun createProfile(
+    oldAabFilePath: String,
+    newAabFilePath: String,
+    buildFlavor: String = "dev"
+  ): ComputeAabDifferences.AabProfile {
+    return ComputeAabDifferences.AabProfile(buildFlavor, oldAabFilePath, newAabFilePath)
+  }
 }
