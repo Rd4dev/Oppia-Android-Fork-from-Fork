aaee6e06804b623c12ad6304213d4b3cb0561148
Dev4rd <dev00004dev@gmail.com>
2024-08-19 00:59:21 +0530

    Update CoverageReporter.kt

--- scripts/src/java/org/oppia/android/scripts/coverage/reporter/CoverageReporter.kt
+++ scripts/src/java/org/oppia/android/scripts/coverage/reporter/CoverageReporter.kt
@@ -9,7 +9,7 @@ import java.io.File
 /** Minimum coverage percentage required. */
 const val MIN_THRESHOLD = 70
 
-/* ANSI escape codes for colors. */ Coverage Reporter
+/* ANSI escape codes for colors. */
 
 /** Green text. */
 const val GREEN = "\u001B[32m"
