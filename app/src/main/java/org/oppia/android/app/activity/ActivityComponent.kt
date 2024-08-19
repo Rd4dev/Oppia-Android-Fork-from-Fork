389ab085e38ebf1cb469ec8db821a1dc681060ac
Dev4rd <dev00004dev@gmail.com>
2024-08-19 09:06:55 +0530

    TEST FILE EXEMPTED ActivityComponent

--- app/src/main/java/org/oppia/android/app/activity/ActivityComponent.kt
+++ app/src/main/java/org/oppia/android/app/activity/ActivityComponent.kt
@@ -5,7 +5,7 @@ import org.oppia.android.app.utility.datetime.DateTimeUtil
 
 /**
  * Root subcomponent for all activities.
- *
+ * ActivityComponent
  * Instances of this subcomponent should be created using [ActivityComponentFactory].
  */
 interface ActivityComponent : AppLanguageActivityInjector, DateTimeUtil.Injector
