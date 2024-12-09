d39b6cf24e1c01c5a72d3ff0ff443ed5db27a6e5
RD Rama Devi <122200035+Rd4dev@users.noreply.github.com>
2024-12-09 13:11:04 +0530

    Update TestFileCheck.kt

--- scripts/src/java/org/oppia/android/scripts/testfile/TestFileCheck.kt
+++ scripts/src/java/org/oppia/android/scripts/testfile/TestFileCheck.kt
@@ -7,7 +7,7 @@ import java.io.FileInputStream
 
 /**
  * Script for ensuring that all production files have test files present.
- * TestFileCheck cc
+ * TestFileCheck cc 2
  * Usage:
  *   bazel run //scripts:test_file_check -- <path_to_directory_root>
  *
